b866d3232c139b164c99588f95ad3f89a68d6c80
EricJerez <ejere004@fiu.edu>
2019-05-11 11:49:13 -0400

    Testing with problem set 1, question1. The third option tries to add an int with a float, which gives the following error: "The type 'float' does not match the type 'int' "

--- COP4555/COP4555/Code.fs
+++ COP4555/COP4555/Code.fs
@@ -1,7 +1,23 @@
 ﻿module Code
 
-module Practice1 =
-    let pred n = n - 1 
+    module Practice1 =
+        let pred n = n - 1 
 
-    let test () = 
-        printfn "The predecesor of 16 is %d" (pred 16) 
+        let test () = 
+          printfn "The predecesor of 16 is %d" (pred 16) 
+
+    module ProblemSet1Q1 =
+
+        let test1 () = 
+            printfn "The result of 2 + 5 * 10 is %d" (2 + 5 * 10) 
+
+        let test2 () = 
+            printfn "The result of 10I * 20I is %A" (10I * 20I)
+    
+        let test3 () = 
+            printfn "The result of 4 + 5.6 is %d" (4 + 5.6)
+
+        let test4() =
+            let str1 = "4"
+            let str2 = "5"
+            printfn "The string is: %s" (str1 + str2)
--- COP4555/COP4555/Program.fs
+++ COP4555/COP4555/Program.fs
@@ -4,8 +4,13 @@
 let main argv = 
     printfn "%A" argv
 
-    Code.Practice1.test()
+    //Code.Practice1.test()
 
+    Code.ProblemSet1Q1.test1()
+    Code.ProblemSet1Q1.test2()
+    Code.ProblemSet1Q1.test3()
+    Code.ProblemSet1Q1.test4()
+   
     printfn "Press ENTER to continue..."
     System.Console.ReadLine() |> ignore
     0 // return an integer exit code
