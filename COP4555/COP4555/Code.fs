5d476751462c3166076bd15f5ffbde99b1be9086
EricJerez <ejere004@fiu.edu>
2019-05-11 11:52:31 -0400

    Created a script and tested a simple addition 4 + 3

--- COP4555/COP4555/Code.fs
+++ COP4555/COP4555/Code.fs
@@ -15,9 +15,10 @@
             printfn "The result of 10I * 20I is %A" (10I * 20I)
     
         let test3 () = 
-            printfn "The result of 4 + 5.6 is %d" (4 + 5.6)
+            printfn "The result of 4 + 5.6 is %d" (4 + 5)
 
         let test4() =
             let str1 = "4"
             let str2 = "5"
             printfn "The string is: %s" (str1 + str2)
+    
