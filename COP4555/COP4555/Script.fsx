bf8a2db6b583a16d7c74c538da5648a4e08520ce
EricJerez <ejere004@fiu.edu>
2019-05-12 02:04:19 -0400

    Created function add that takes 2 lists and add the elements in the same position. (Imcomplete pattern matches warning)

--- COP4555/COP4555/Script.fsx
+++ COP4555/COP4555/Script.fsx
@@ -2,9 +2,10 @@
 let list1 = [1;2;3;4;5]
 let list2 = [6;7;8;9;10]
 
-let rec sum list = 
-    match list with
-    | [] -> 0
-    | x :: xs -> x + sum xs
 
-sum list1
+let rec add a b =
+    match a, b with
+    | [], [] -> []
+    | a::atail, b::btail -> (a + b) :: (add atail btail) 
+
+add list1 list2
