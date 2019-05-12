e7f93a32a01c61eff7e7395731feeaea8633537e
EricJerez <ejere004@fiu.edu>
2019-05-12 01:53:05 -0400

    Creating a list and a method to add the elements of that list in a recursive way. Error when trying to define a method in code.fs and call it in program.fs : "The result of this expression is implicitly ignored. Consider using 'ignore' e.g. 'expr |> ignore', or ' e.g. 'let result = expr' " Currently working in my script file where the functions are called correclty.

--- COP4555/COP4555/Script.fsx
+++ COP4555/COP4555/Script.fsx
@@ -1,19 +1,10 @@
 ﻿
-let rec fact1 x =
-    if x < 1 then 1
-    else x * fact1 (x - 1)
+let list1 = [1;2;3;4;5]
+let list2 = [6;7;8;9;10]
 
-fact1 4
+let rec sum list = 
+    match list with
+    | [] -> 0
+    | x :: xs -> x + sum xs
 
-let rec fact2 x =
-    match x with
-    | 0 -> 1
-    | 1 -> 1
-    | x -> x * fact1 (x-1)
-
-fact2 4
-
-let rec fact3 = 
-    fun x -> x * fact1 (x-1)
-
-fact3 4
+sum list1
