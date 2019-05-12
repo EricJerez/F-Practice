5d1421fcce04631d888bb44394f5ea02b5a4ee73
EricJerez <ejere004@fiu.edu>
2019-05-12 02:17:09 -0400

    Fixed the error and now the method successfully adds the corresponding elements of two lists.

--- COP4555/COP4555/Script.fsx
+++ COP4555/COP4555/Script.fsx
@@ -5,7 +5,8 @@ let list2 = [6;7;8;9;10]
 
 let rec add a b =
     match a, b with
-    | [], [] -> []
+    | _ , [] -> []
+    | [] , _ -> []
     | a::atail, b::btail -> (a + b) :: (add atail btail) 
 
 add list1 list2
